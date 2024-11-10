e3c3d7f7b134dc83306391f9dd3e2b00b7dfbf6a
avraham <2721avi@gmail.com>
2024-11-10 11:51:48 +0200

    empty controllers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import "dotenv/config";
-const port = process.env.PORT||2000;
+import adminController from "./controllers/adminController";
+import usersController from "./controllers/usersController";
+import candidatesController from "./controllers/candidatesController";
+import votesController from "./controllers/votesController";
+
+const PORT = process.env.PORT || 2000;
 const app = express();
-app.listen(port, () => {
-  console.log(`server is visit "http://localhost:${port}"`);
+
+app.use("/api/users", usersController);
+app.use("/api/admin", adminController);
+app.use("/api/votes", votesController);
+app.use("/api/candidates", candidatesController);
+app.listen(PORT, () => {
+  console.log(`server is visit "http://localhost:${PORT}"`);
 });
